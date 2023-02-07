6fc076625e14c3a98c99081da5cda6b821d86965
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-06 23:14:41 -0500

    Adding Profiles Example

---
+++ src/test/java/guru/springframework/spring6di/controllers/i18n/myi18NControllerTestEN.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6di.controllers.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Donald F. Coffin
+ */
+@ActiveProfiles("EN")
+@SpringBootTest
+class myi18NControllerTestEN {
+
+	@Autowired
+	Myi18NController myi18NController;
+
+	@Test
+	void sayHello() {
+
+		System.out.println(myi18NController.sayHello());
+	}
+}
